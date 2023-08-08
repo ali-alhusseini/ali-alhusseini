8511436e4d85b4600e25cbc1674e2f1a183ed151
Ali Alhusseini <hello@alialhusseini.com>
2023-08-07 22:37:46 -0300

    Add alternating switches for modes

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
 document.addEventListener('mousemove', function(event) {
     var cursor = document.getElementById('cursor');
-    var xPos = event.clientX - cursor.offsetWidth / 2;
-    var yPos = event.clientY - cursor.offsetHeight / 2;
-    cursor.style.left = xPos + 'px';
-    cursor.style.top = yPos + 'px';
+    var xPosition = event.clientX - cursor.offsetWidth / 2;
+    var yPosition = event.clientY - cursor.offsetHeight / 2;
+    cursor.style.left = xPosition + 'px';
+    cursor.style.top = yPosition + 'px';
 });
 
+const lightModeBtn = document.getElementById('light-mode-btn');
+const darkModeBtn = document.getElementById('dark-mode-btn');
+
+lightModeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+    darkModeBtn.style.display = 'block';
+    lightModeBtn.style.display = 'none';
+});
+
+darkModeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+    darkModeBtn.style.display = 'none';
+    lightModeBtn.style.display = 'block';
+});
