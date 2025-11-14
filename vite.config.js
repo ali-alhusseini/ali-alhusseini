a488317e5a5a09291a5c03b768e7dc88c872f1fa
Ali Alhusseini <61102155+ali-alhusseini@users.noreply.github.com>
2025-11-14 04:17:50 -0400

    Set project root to current directory

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  root: '.',
   base: '/ali-alhusseini/',
   plugins: [tailwindcss()],
 });
