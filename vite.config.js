2db9fb2ddcdc42164c9a5a74586ea060cc18c28f
Ali Alhusseini <61102155+ali-alhusseini@users.noreply.github.com>
2025-11-14 04:13:12 -0400

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/REPO-NAME/',
+  base: '/ali-alhusseini/',
   plugins: [tailwindcss()],
 });
