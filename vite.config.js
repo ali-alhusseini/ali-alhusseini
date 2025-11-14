a7996ca7bb10874f98731934d43d2a2f80a85a04
Ali Alhusseini <hello@alialhusseini.com>
2025-11-14 12:44:10 -0400

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ali-alhusseini/',
+  base: '/',
   plugins: [tailwindcss()],
 })
